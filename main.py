bba2f72569d128c75f0e7b61f9767a3f77b1207b
dkq <dev.dkq@gmail.com>
2023-07-19 00:32:11 +0900

    edit

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from rule import rule_A, rule_B
-import random
-#a = 분자 b = 분모
-a = random.randint(1,10000000)
-b = random.randint(1,10000000)
+# a = 분자 b = 분모
+a = int(input("분자: "))
+b = int(input("분모: "))
 
 arr = []
 i=0
 print(f"{a}/{b}")
-print("\n\n")
 while True:
-#    print(f"{a}/{b}")
     if(a > 0):
         a, b = rule_B(a,b)
         print(f"B: {a}/{b}")
